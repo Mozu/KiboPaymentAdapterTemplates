29ed2d92ec8870a667ce80466f5cb42b10635437
perry.munger <perry.munger@kibocommerce.com>
2018-05-02 18:12:47 -0500

    Forgot to make it respond to any AdapterContext object

--- src/main/java/com/kibo/pegateway/dto/override/AdapterContextOverrideDeserializerModifier.java
+++ src/main/java/com/kibo/pegateway/dto/override/AdapterContextOverrideDeserializerModifier.java
@@ -5,11 +5,12 @@ import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.mozu.api.contracts.paymentservice.extensibility.v1.AdapterContext;
 
 public class AdapterContextOverrideDeserializerModifier extends BeanDeserializerModifier {
     @Override
     public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
-        if (beanDesc.getBeanClass().isAssignableFrom(AdapterContextOverride.class)) {
+        if (beanDesc.getBeanClass().isAssignableFrom(AdapterContext.class)) {
             return new AdapterContextOverrideDeserializer((JsonDeserializer<Object>) deserializer);
         }
         return deserializer;
--- src/main/java/com/kibo/pegateway/dto/override/AdapterContextOverrideSerializerModifier.java
+++ src/main/java/com/kibo/pegateway/dto/override/AdapterContextOverrideSerializerModifier.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.mozu.api.contracts.paymentservice.extensibility.v1.AdapterContext;
 
 public class AdapterContextOverrideSerializerModifier extends BeanSerializerModifier {
     @Override
     public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc, JsonSerializer<?> serializer) {
-        if (beanDesc.getBeanClass().isAssignableFrom(AdapterContextOverride.class)) {
+        if (beanDesc.getBeanClass().isAssignableFrom(AdapterContext.class)) {
             return new AdapterContextOverrideSerializer((JsonSerializer<Object>) serializer);
         }
         return serializer;
